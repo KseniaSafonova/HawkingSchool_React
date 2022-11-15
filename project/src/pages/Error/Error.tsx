9139ed9eb1e26ff65696b169a6b821de72bd06e3
KseniaSafonova <kseniatsn@gmail.com>
2022-11-15 13:42:45 +0300

    error page

--- project/src/pages/Error/Error.tsx
+++ project/src/pages/Error/Error.tsx
@@ -2,8 +2,9 @@ import Header from "../../components/Header";
 import HeaderNav from "../../components/HeaderNav";
 import Footer from "../../components/Footer";
 import styles from '../../styles/Error/Error.module.css';
-import topImage from '../../images/topImage.png'
-
+import error from '../../images/error.svg';
+import pointsLeft from '../../images/pointsLeft.png';
+import pointsRight from '../../images/pointsRight.png';
 
 const Error = () => {
     return (
@@ -13,12 +14,16 @@ const Error = () => {
             <div className={styles.container}>
                 <div className={styles.backgroundImg}></div>
                 <div className={styles.topImg}></div>
-                <div className={styles.content}>
+                <div className={styles.pointsLeft}><img src={pointsLeft} /></div>
+                <div className={styles.contentLeft}>
                     <div className={styles.error}>Ошибка 404</div>
                     <div className={styles.description}>Возможно, у вас опечатка в адресе страницы, или её просто не существует</div>
                     <div className={styles.button}><button>Вернуться на главную</button></div>
-                    <div>404</div>
                 </div>
+                <div className={styles.contentRight}>
+                    <div className={styles.errorNumber}><img src={error} alt="" /></div>
+                </div>
+                <div className={styles.pointsRight}><img src={pointsRight} /></div>
             </div>
             <Footer />
         </>
