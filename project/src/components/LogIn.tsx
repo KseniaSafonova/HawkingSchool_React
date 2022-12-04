88ac7092479f4bd14e11cd34cbcdf0e46e60d0ce
KseniaSafonova <kseniatsn@gmail.com>
2022-12-04 15:32:39 +0300

    form

--- project/src/components/LogIn.tsx
+++ project/src/components/LogIn.tsx
@@ -7,15 +7,23 @@ const LogIn = () => {
                 <div className={styles.backgroundImg}></div>
                 <div className={styles.topImg}></div>
                 <div className={styles.content}>
-                    <div>Авторизация</div>
-                    <div>Авторизируйтесь, чтобы начать
+                    <div className={styles.title}>Авторизация</div>
+                    <div className={styles.description}>Авторизируйтесь, чтобы начать
                         публиковать свои объявления</div>
-                    <div><input type="text" /></div>
-                    <div><input type="text" /></div>
-                    Запомнить меня
-                    <a href="">Забыли пароль?</a>
+                    <div className={styles.input}>
+                        <input type="text" placeholder='Логин' />
+                        <input type="text" placeholder='Пароль' />
+                    </div>
+                    <div className={styles.password}>
+                        <label className={styles.switch}>
+                            <input type="checkbox" className={styles.checkbox} />
+                            <span className={styles.slider}></span>
+                        </label>
+                        <span className={styles.rememberMe}>Запомнить меня</span>
+                        <a href="">Забыли пароль?</a>
+                    </div>
                     <div><button>Войти</button></div>
-                    Еще нет аккаунта? <a href="">Создайте акканут</a>
+                    <div className={styles.createAccount}><span>Еще нет аккаунта?</span>  <a href="">Создайте акканут</a></div>
                 </div>
             </div>
         </>
